b804e4b7b9bdcb5ec1ac8ee791b516e29dc3ee00
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-02-13 15:14:43 +0530

    completed basic part 1

---
+++ basic_01/06_string.js
@@ -0,0 +1,33 @@
+const name = "himanshu"
+const name2 = new String("himanshu");
+const repoCount = 50
+
+// console.log(name + repoCount+" Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+console.log(name2.length);
+console.log(name2.toUpperCase());
+console.log(name2.indexOf("h"))
+
+console.log(name2.substring(0,4))
+
+console.log(name2.slice(-7,4));
+
+const newStringOne = " himanshu "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+console.log("himanshu".replace("hi","by"))
+
+console.log(name2.includes("hi"))
+
+
+const str = "hello-i-am-Himanshu-Khairnar"
+
+console.log(str.split("-"))
+let x = Boolean("true");
+let x1 = Boolean("False");
+let x2 = Boolean(300);
+let x3 = true;
+console.table([x,x1,x2,x3])
